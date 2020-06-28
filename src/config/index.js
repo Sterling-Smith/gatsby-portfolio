1f161e31dc80a928e072b03edda853ac76f981a5
Sterling Smith <iam@sterlingsmith.me>
2020-06-28 08:34:47 +0000

    updated medium rss feed

--- src/config/index.js
+++ src/config/index.js
@@ -4,18 +4,17 @@ module.exports = {
     siteTitle: "Sterling Smith Portfolio",
     siteShortTitle: "ss.", // Used as logo text in header, footer, and splash screen
     siteDescription: "Hi there! I'm Sterling Smith, a fractional product manager, startup founder and freelance app developer based in Austin.",
-    siteUrl: "https://sterlingsmith.me",
+    siteUrl: "https://sterling.ooo",
     siteLanguage: "en_US",
     siteIcon: "src/content/favicon.png", // Relative to gatsby-config file
 
-    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantin.muenster&api_key=51t0yukvezbsj6vz5h46icya39agdbxw1snykm3v",
-    shownArticles: 3,
+    // mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantin.muenster&api_key=51t0yukvezbsj6vz5h46icya39agdbxw1snykm3v",
+    // shownArticles: 3,
+
+    mediumRssFeed: "https://medium.com/feed/@sterlingwrites",
+    shownArticles:3,
     
     socialMedia: [
-        {
-            name: "Xing",
-            url: "https://www.xing.com/profile/Konstantin_Muenster"
-        },
         {
             name: "Linkedin",
             url: "https://www.linkedin.com/in/sterlingssmith/"
